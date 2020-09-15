bbed858cbefd6c4efd0435c282af0cfb8251a3dc
Graham Howden <graham_howden1@yahoo.co.uk>
2020-09-15 18:45:56 +0100

    Updates

---
+++ precache-manifest.32d727ce1ff803702b1e0c473c0ad0cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2115bb52cb4f7eabd8081416e986013d",
+    "url": "/and-challenge-five/index.html"
+  },
+  {
+    "revision": "470180b110dcd98c1b0f",
+    "url": "/and-challenge-five/static/js/2.2811259c.chunk.js"
+  },
+  {
+    "revision": "6ee52e9cd3f81fb125d1889debfd0437",
+    "url": "/and-challenge-five/static/js/2.2811259c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809b145574b4249f4262",
+    "url": "/and-challenge-five/static/js/main.322c801a.chunk.js"
+  },
+  {
+    "revision": "ffee33c4a3fb049b25c6",
+    "url": "/and-challenge-five/static/js/runtime-main.3ed0e3b4.js"
+  }
+]);
